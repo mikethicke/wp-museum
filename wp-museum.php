df73e6b3f8b6eb95660e91bc7c84ad652152537c
mikethicke <mikethicke@gmail.com>
2020-08-26 09:48:08 -0400

    version update

--- wp-museum.php
+++ wp-museum.php
@@ -8,7 +8,7 @@
  * @wordpress-plugin
  * Plugin Name: Museum for WordPress
  * Description: Manages a database of museum objects.
- * Version: 0.6.7
+ * Version: 0.6.8
  * Author: Mike Thicke
  * Author URI: http://www.mikethicke.com
  * Text Domain: wp-museum
