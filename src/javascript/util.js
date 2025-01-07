0ffc41e692e3da02aa35dbf63ecb82dfee63d79b
Mike Thicke <mikethicke@gmail.com>
2025-01-07 17:47:49 -0500

    small cleanup

--- src/javascript/util.js
+++ src/javascript/util.js
@@ -11,9 +11,6 @@ export const baseRestPath = '/wp-museum/v1';
 export const wordPressRestBase = '/wp/v2'
 
 // https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
-/**
- * 
- */
 export function hexToRgb(hex) {
 	// Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
 	var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
